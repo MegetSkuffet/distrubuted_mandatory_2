9edf593bac583c7e7e1334b9a18466ffb94f5c19
Malte Alexander Koefoed Schalck <malsc@itu.dk>
2022-09-21 15:59:17 +0200

    lets go gamoider

---
+++ TCP-IP.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var server struct{
+	recieve := make(chan string, 1)
+	
+}
+func main() {
+
+	
+	/* for {
+		fmt.Println("pls input command")
+
+		var command,err = fmt.Scanln()
+		_ = err
+
+		switch command {
+        case "":
+            fmt.Println("1")
+		}
+
+		
+	} */
+}
