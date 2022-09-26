d3ef3e078065a10830a44b190d609d43956e532e
Jklo <jklo@itu.dk>
2022-09-26 14:38:19 +0200

    håndtere message lost

--- TCP-IP.go
+++ TCP-IP.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"hash/fnv"
 	"os"
 	"time"
 )
@@ -18,11 +19,13 @@ type client struct {
 }
 
 type packet struct {
-	to      string
-	from    client
-	message string
-	sync    int
-	ack     int
+	to           string
+	from         client
+	message      string
+	sync         int
+	originalSync int
+	ack          int
+	hash         uint32
 }
 
 func main() {
@@ -42,6 +45,7 @@ func main() {
 	switch commandAsString {
 	case "send message":
 		var packet = packet{from: client, sync: 1, ack: 0}
+		packet.originalSync = packet.sync
 		server.receive <- packet
 	}
 
@@ -56,14 +60,27 @@ func startServer(s server) {
 				if p.message == "" {
 					//Packet is handshake
 					fmt.Println("server received sync", p.sync)
+					time.Sleep(1 * time.Second)
 					p.sync++
 					p.ack = p.sync
 					fmt.Println("server is sending sync", p.sync, "and acknowlegdement", p.ack)
 					p.from.receive <- p
 				} else {
 					//Packet has message after handshake
+					localHash := hash(p.message)
+
 					fmt.Println("server received sync", p.sync, "and acknowlegdement", p.ack)
-					fmt.Println("Message received: ", p.message)
+
+					if p.hash != localHash {
+						fmt.Println("Shits fucked")
+					} else {
+
+						fmt.Println("## Hash is correct ##")
+						fmt.Println("Message received: ", p.message)
+						p.message = "Confirm"
+						p.from.receive <- p
+					}
+
 				}
 			}
 
@@ -74,21 +91,46 @@ func startServer(s server) {
 }
 
 func birthClient(c client, s server) {
+	var timeout int
 	for {
 		select {
 		case p := <-c.receive:
 			{
-				fmt.Println("client received ackknowlegdement", p.ack, "and sync", p.sync)
-				if p.ack == 2 {
-					p.sync++
+				if p.ack == p.originalSync+1 && p.message != "Confirm" {
+					fmt.Println("client received ackknowlegdement", p.ack, "and sync", p.sync)
+
 					p.ack = p.sync
+
+					p.sync++
 					fmt.Println("What message do you wish to send?")
 					sc := bufio.NewScanner(os.Stdin)
 					sc.Scan()
 					p.message = sc.Text()
+					p.hash = hash(p.message)
 
 					fmt.Println("client is sending sync", p.sync, "and acknowlegdement", p.ack, "and message \"", p.message, "\"")
 					s.receive <- p
+
+				}
+				for i := 0; i < 6; i++ {
+
+					select {
+					case <-c.receive:
+						fmt.Println("Client received answer correctly")
+						break
+					default:
+						break
+					}
+
+					if i == 5 {
+						s.receive <- p
+						i = 0
+						timeout++
+					} else if timeout == 3 {
+						fmt.Println("could not send message")
+						os.Exit(0)
+					}
+
 				}
 			}
 
@@ -102,4 +144,10 @@ func clientSendMessage(c client, s server, msg string) {
 	var packet = packet{from: c, sync: 1, ack: 0}
 	s.receive <- packet
 
+}
+
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
 }
